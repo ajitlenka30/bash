a8ee3e590cf106ac24742abe6847047946e135b9
ajitlenka30 <ajitlenka30@gmail.com>
2025-03-06 06:58:01 +0000

    var variable with cmd used

---
+++ var2.sh
@@ -0,0 +1,10 @@
+#! bin\bash
+
+<<end
+to use cmd in a var we needed $(inside cmd)
+
+end
+name=$(hostname)
+username=$(whoami)
+
+echo "name of this pc is :- $name and current user is :- $username"
