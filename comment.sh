563df3228969353984d3baae27fb7148c37bb2e7
ajitlenka30 <ajitlenka30@gmail.com>
2025-03-06 05:46:51 +0000

    comments in bash

---
+++ comment.sh
@@ -0,0 +1,13 @@
+#! \bin\bash\
+
+
+# first method of comments
+
+<< comments # we have to use same word on starting and ending
+
+you can write any thing
+
+# on end also will be same line
+comments
+
+echo "hiiiiiii"
