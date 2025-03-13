3fcaa5f9fa2e7fdca33820724f7f16859d15068d
ajitlenka30 <ajitlenka30@gmail.com>
2025-03-13 16:14:47 +0530

    test

--- case.sh
+++ case.sh
@@ -5,12 +5,15 @@ echo "current options"
 echo "a for date"
 echo "b for host name"
 echo "c for htop"
+echo "d for new user"
 
-read choice
+read -p "Enter your choice :- " choice
 
 case $choice in
 	a)date;;
 	b)hostname;; 
 	c)hotop;;
+	d)read -p "Enter user name:- " name
+	  sudo adduser -m $name;;
 	*)echo "enter a valid choice sur..."
 esac
